0a41e92096c81521ebf3fad04ae6054fb8ad3909
Levi Hechenberger Di Valentino <68319554+levvij@users.noreply.github.com>
2021-10-11 18:49:55 +0200

    add upload

---
+++ luucy/upload.d.ts
@@ -0,0 +1,21 @@
+/** Uplaod handler */
+declare interface Upload {
+    /** Creates an upload listener. The plugin will be listed in the upload panel */
+    /** @param pattern specifies which files will be sent to the plugin. Use '*' as a wildcard (e.g. '*.txt', 'exported_model_*.txt') */
+    constructor(pattern: string);
+
+    /** Is called whenever a matching file is uploaded */
+    onupload: PluginEvent<File>;
+}
+
+/** File */
+declare interface File {
+    /** File name */
+    name: string;
+
+    /** Size in bytes */
+    size: number;
+
+    /** Reads the file as a string */
+    toString(): Promise<string>;
+}
