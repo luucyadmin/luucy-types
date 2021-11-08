6d0b1fa6752a60829cceb4f31bfdfa61d711d822
Levi Hechenberger Di Valentino <68319554+levvij@users.noreply.github.com>
2021-11-08 14:57:08 +0100

    add array.include to global types

--- globals/array.d.ts
+++ globals/array.d.ts
@@ -180,6 +180,13 @@ interface Array<T> {
     reduceRight<U>(callbackfn: (previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U, initialValue: U): U;
 
     [n: number]: T;
+
+    /**
+     * Determines whether an array includes a certain element, returning true or false as appropriate.
+     * @param searchElement The element to search for.
+     * @param fromIndex The position in this array at which to begin searching for searchElement.
+     */
+     includes(searchElement: T, fromIndex?: number): boolean;
 }
 
 interface ConcatArray<T> {
