17726e87e5874c19c3cc5b6a4a7ee43b4a26153c
Levi Hechenberger Di Valentino <68319554+levvij@users.noreply.github.com>
2021-11-16 05:54:43 +0100

    fix imports

--- globals/index.d.ts
+++ globals/index.d.ts
@@ -7,7 +7,6 @@
 /// <reference path="json.d.ts" />
 /// <reference path="math.d.ts" />
 /// <reference path="number.d.ts" />
-/// <reference path="promise.d.ts" />
 /// <reference path="regex.d.ts" />
 /// <reference path="string.d.ts" />
 /// <reference path="object.d.ts" />
