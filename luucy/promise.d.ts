459e08f267ef1a7bfdfd6eb7752e0b60c37d38ce
Levi Hechenberger Di Valentino <68319554+levvij@users.noreply.github.com>
2021-11-16 05:56:31 +0100

    add promise constructor

--- luucy/promise.d.ts
+++ luucy/promise.d.ts
@@ -1,4 +1,6 @@
 declare class Promise<T> {
+    constructor(resolver: (done: (result: T) => void) => void);
+
     static resolve<T>(value: T): Promise<T>;
 
     then<TRes>(handler: (value: T) => TRes): Promise<TRes>;
