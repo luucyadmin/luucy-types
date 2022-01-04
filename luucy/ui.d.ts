560afc667275c7c0eb5326fe33a096bd6565382e
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-01-04 10:04:38 +0100

    add separator

--- luucy/ui.d.ts
+++ luucy/ui.d.ts
@@ -379,6 +379,19 @@ declare namespace ui {
         remove(child: Element);
     }
 
+    /**
+     * Separator
+     * 
+     * Splits content with a horizontal line, compareable to <hr>
+     * 
+     * @example // Separate two ares in a section
+     * const section = new ui.Section("Important Details");
+     * section.add(new ui.Label("Lorem"));
+     * section.add(new ui.Separator());
+     * section.add(new ui.Label("Ipsum"));
+     */
+    class Separator extends Element {}
+
     /** 
      * Panel 
      * 
