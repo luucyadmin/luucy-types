6ebc8f0e555cc6c6b1e0ac1d0819c7c8228738bf
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-01-19 12:28:16 +0100

    fix explicit column type

--- luucy/ui.d.ts
+++ luucy/ui.d.ts
@@ -909,10 +909,10 @@ declare namespace ui {
      * new ui.Table<Building>(buildings, [
      *     new ui.Column("Image", building => new ui.Image(building.imageUrl)),
      *     new ui.Column("Name", building => building.name),
-     *     new ui.Column("Year", building => building.builtAt.getFullYear()).addTooltip(building => building.builtAt.toLocalDateString()),
+     *     new ui.Column<Building>("Year", building => building.builtAt.getFullYear()).addTooltip(building => building.builtAt.toLocalDateString()),
      * 
      *     // add a report column with an action, which, when clicked, will open a modal
-     *     new ui.Column("Report", building.lastReport.name).addAction(ui.icons.file, "View Report", building => {
+     *     new ui.Column<Building>("Report", building.lastReport.name).addAction(ui.icons.file, "View Report", building => {
      *         const modal = new ui.Modal();
      *         // add whatever needs to be added to your modal!
      * 
@@ -980,7 +980,7 @@ declare namespace ui {
          * section.add(table);
          * 
          * section.add(new ui.Button("Add Lucerene Main Station", () => {
-         *     const lucerneMainStation = { name: "Lucerne Main Station" };
+         *     const lucerneMainStation: Building = { name: "Lucerne Main Station" };
          *     
          *     table.addRecord(lucerneMainStation);
          * }));
