ce28e535d082cf453aa25451bf85aecc06b6edea
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-01-19 19:17:06 +0100

    fix remove request reference

--- luucy/index.d.ts
+++ luucy/index.d.ts
@@ -1,7 +1,6 @@
 /// <reference path="promise.d.ts" />
 /// <reference path="console.d.ts" />
 /// <reference path="event.d.ts" />
-/// <reference path="request.d.ts" />
 /// <reference path="storage.d.ts" />
 /// <reference path="timer.d.ts" />
 
