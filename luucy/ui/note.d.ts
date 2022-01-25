b5d3ac78f95dbca5b54b40f8dc8c7e33e91a91f6
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-01-25 11:04:16 +0100

    add note events

--- luucy/ui/note.d.ts
+++ luucy/ui/note.d.ts
@@ -6,6 +6,30 @@ declare namespace ui {
      * section.add(new ui.Note(ui.danger, "This did not go well!"));
      */
     class Note extends Element {
-        constructor(state: State, message?: string);
+        constructor(state: State, message: string);
+
+        /**
+         * Defines the Notes state
+         * 
+         * Can be changed after beeing added to an area/element and will automatically update.
+         * 
+         * @example // Change a notes state
+         * const note = new ui.Note(ui.danger, 'Example Note');
+         * section.add(note);
+         * 
+         * section.add(new ui.Button('change note state!', () => {
+         *     note.state = ui.success;
+         * }));
+         */
+        state: State;
+
+        /**
+         * Defines the Notes state
+         * 
+         * Can be changed after beeing added to an area/element and will automatically update.
+         */
+        message: string;
+
+        onContentChange: Event<string>;
     }
 }
