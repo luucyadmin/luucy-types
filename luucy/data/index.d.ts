2ce5d981ec539c40dcac4cfd1ceb2b483fd6c93e
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-01-26 12:49:40 +0100

    add references

--- luucy/data/index.d.ts
+++ luucy/data/index.d.ts
@@ -1,7 +1,11 @@
+/// <reference path="building.d.ts" />
+/// <reference path="organization.d.ts" />
 /// <reference path="project.d.ts" />
+/// <reference path="usage.d.ts" />
 /// <reference path="variant.d.ts" />
+/// <reference path="wall.d.ts" />
 
 /**
- * Contains all the classes that luucy exposes 
+ * Contains all the data-related classes that luucy exposes 
  */
 declare namespace data {}
