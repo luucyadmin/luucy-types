de91c606ba9c44b96d6308408cdaebc764375069
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-02-07 06:02:00 +0100

    add wall doc

--- luucy/data/wall.d.ts
+++ luucy/data/wall.d.ts
@@ -1,8 +1,28 @@
 declare namespace data {
+    /**
+     * Building Wall
+     * 
+     * A buildings walls can be calculated by calling `measureWalls()` on `Building`.
+     */
     class Wall {
+        /**
+         * Length of the wall
+         */
         readonly length: number;
+
+        /**
+         * Height of the wall
+         */
         readonly height: number;
+
+        /**
+         * Area of the wall
+         */
         readonly area: number;
+
+        /**
+         * Angle in radians of the wall
+         */
         readonly angle: number;
     }
 }
