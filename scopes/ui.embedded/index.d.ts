88d29588883c0d4cdf1a5138dba916720d888402
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-02-11 10:52:57 +0100

    add dom reference

--- scopes/ui.embedded/index.d.ts
+++ scopes/ui.embedded/index.d.ts
@@ -1,8 +1,10 @@
+/// <reference path="dom.d.ts" />
+
 declare namespace ui.embedded {
     abstract class Host {
         root: ui.embedded.Element;
 
-        addStyle(selector: string, css: { [key: string]: any }): this;
+        addStyle(selector: string, rules: { [key: string]: any }): this;
     }
 
     function enable(): Host;
