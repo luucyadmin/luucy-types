21d04111e2e08d1a612544b5c71e68625c6f4aa7
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-03-14 15:35:10 +0100

    add presentation.ui

---
+++ scopes/presentation.ui/index.ts
@@ -0,0 +1,12 @@
+/// <reference path="../ui/index.d.ts" />
+/// <reference path="../presentation/index.d.ts" />
+
+declare namespace presentation {
+    /**
+     * Creates a panel for presentations
+     * 
+     * A button with the provided icon will be visible next to the book icon. 
+     * The render function will be called whenever the slides change and a empty container will be provided.
+     */
+    function createPanel(icon: string, name: string, render: (container: ui.ElementContainer, slide: Slide) => void);
+}
