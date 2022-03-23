71c1e24cad018d4f9d96d49acb5527aec12d7108
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-03-23 19:45:40 +0100

    add toolbar demo

--- scopes/ui.toolbar/index.d.ts
+++ scopes/ui.toolbar/index.d.ts
@@ -1,4 +1,28 @@
 declare namespace ui.toolbar {
+    /**
+     * Toolbar Panel
+     * 
+     * @example // Create Marker Panel
+     * ui.toolbar.createProjectTool(ui.icons.pointOfInterest, 'Project', panel => {
+     *     const markers: map.Marker[] = [];
+     * 
+     *     panel.add(new ui.Label('Click on the map to add markers'));
+     * 
+     *     panel.onMapClick.subscribe(position => {
+     *         markers.push(new map.Marker(position.flattenedCopy()));
+     *     });
+     * 
+     *     panel.onClose.subscribe(() => {
+     *         for (let marker of markers) {
+     *             marker.remove();
+     *         }
+     *     });
+     * 
+     *     panel.addAction(ui.icons.center, 'Center Points', () => {
+     *         map.focus(markers);
+     *     });
+     * });
+     */
     interface Panel extends ui.ElementContainer {
         onClose: Event<void>;
         close(): void;
