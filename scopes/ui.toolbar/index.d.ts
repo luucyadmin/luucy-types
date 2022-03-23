e30e2566c4f8c71e72826d64ea68d66d2de9b3e2
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-03-23 17:56:25 +0100

    fix toolbar panels

--- scopes/ui.toolbar/index.d.ts
+++ scopes/ui.toolbar/index.d.ts
@@ -1,64 +1,67 @@
 declare namespace ui.toolbar {
+    interface Panel {
+        onClose: Event<void>;
+        close(): void;
+
+        onMapClick: Event<GlobalPosition>;
+
+        addAction(icon: IconElement, name: string, handler: () => void): void;
+    }
+
     /**
      * Adds a button to the project tools
      * 
-     * `onOpen` is called when the user clicks on the tools button.
+     * `render` is called when the user clicks on the tools button.
      * Elements can be added to panel.
-     * Close the panel by calling `close()`
+     * Close the panel by calling `panel.close()`
      * 
      * @example // Create custom project tool panel
-     * ui.toolbar.createProjectTool(ui.icons.account, 'Example Tool', (panel, close) => {
+     * ui.toolbar.createProjectTool(ui.icons.account, 'Example Tool', panel => {
      *     panel.add(new ui.Label('Project Tool'));
      * });
      */
-    function createProjectTool(icon: ui.IconElement, name: string, onOpen: (panel: ui.Container, close: () => void) => void): void;
+    function createProjectTool(icon: ui.IconElement, name: string, render: (panel: ui.toolbar.Panel) => void): void;
 
     /**
      * Adds a button to the layer tools
      * 
-     * `onOpen` is called when the user clicks on the tools button.
+     * `render` is called when the user clicks on the tools button.
      * Elements can be added to panel.
-     * Close the panel by calling `close()`
+     * Close the panel by calling `panel.close()`
      * 
      * @example // Create custom project tool panel
-     * ui.toolbar.createLayerTool(ui.icons.account, 'Example Tool', (panel, close) => {
+     * ui.toolbar.createLayerTool(ui.icons.account, 'Example Tool', panel => {
      *     panel.add(new ui.Label('Layer Tool'));
      * });
      */
-    function createLayerTool(icon: ui.IconElement, name: string, onOpen: (panel: ui.Container, close: () => void) => void): void;
+    function createLayerTool(icon: ui.IconElement, name: string, render: (panel: ui.toolbar.Panel) => void): void;
 
     /**
      * Adds a button to the export tools
      * 
-     * `onOpen` is called when the user clicks on the tools button.
+     * `render` is called when the user clicks on the tools button.
      * Elements can be added to panel.
-     * Close the panel by calling `close()`
+     * Close the panel by calling `panel.close()`
      * 
      * @example // Create custom project tool panel
-     * ui.toolbar.createExportTool(ui.icons.account, 'Example Tool', (panel, close) => {
+     * ui.toolbar.createExportTool(ui.icons.account, 'Example Tool', panel => {
      *     panel.add(new ui.Label('Export Tool'));
      * });
      */
-    function createExportTool(icon: ui.IconElement, name: string, onOpen: (panel: ui.Container, close: () => void) => void): void;
+    function createExportTool(icon: ui.IconElement, name: string, render: (panel: ui.toolbar.Panel) => void): void;
 
     /**
      * Adds a new modeling tool button
      * 
-     * Call `complete()` when your tool is done.
-     * This will close the panel and detach the onClick listener.
-     * 
-     * onAbort will be called when the tool is closed by clicking on the icon.
-     * Remove and modifications made to the map!
-     * 
-     * onClick will be called when the user clicks on the map.
+     * Call `panel.close()` when your tool is done.
      * 
      * @example // Create custom project tool panel
-     * ui.toolbar.createProjectTool(ui.icons.account, 'Example Tool', (panel, complete, abort, click) => {
+     * ui.toolbar.createProjectTool(ui.icons.account, 'Example Tool', panel => {
      *     panel.add(new ui.Label('Create markers by clicking on the map'));
      * 
      *     const markers = [];
      * 
-     *     click.subscribe(position => {
+     *     panel.onMapClick.subscribe(position => {
      *         markers.push(new map.Maker(position));
      * 
      *         // call complete when 5 markers have been added
@@ -67,12 +70,13 @@ declare namespace ui.toolbar {
      *         }
      *     });
      * 
-     *     abort.subscribe(() => {
+     *     // clean up the markers
+     *     panel.onClose.subscribe(() => {
      *         for (let marker of markers) {
      *             marker.remove();
      *         }
      *     });
      * });
      */
-    function createModelingTool(icon: ui.IconElement, name: string, onOpen: (panel: ui.ElementContainer, complete: () => void, onAbort: Event<void>, onClick: Event<GlobalPosition>) => void): void;
+    function createModelingTool(icon: ui.IconElement, name: string, onOpen: (panel: ui.toolbar.Panel) => void): void;
 }
