45c3ce8559b928b8fff1dd834b2906a2cba1dbdb
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-05-06 14:02:50 +0200

    fix promise handler requirement

--- scopes/core/promise.d.ts
+++ scopes/core/promise.d.ts
@@ -55,7 +55,7 @@ declare class Promise<T> {
      *     console.log(result); // -> 1
      * })
      */
-    then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | Promise<TResult1>) | undefined | null): Promise<TResult1 | TResult2>;
+    then<TResult1 = T, TResult2 = never>(onfulfilled: ((value: T) => TResult1 | Promise<TResult1>) | undefined | null): Promise<TResult1 | TResult2>;
      
     /**
      * Subscribe to when the promise fails.
@@ -72,5 +72,5 @@ declare class Promise<T> {
      *     console.log(error); // -> Some Error
      * })
      */
-    catch<TResult = never>(onrejected?: ((reason: any) => TResult | Promise<TResult>) | undefined | null): Promise<T | TResult>;
+    catch<TResult = never>(onrejected: ((reason: any) => TResult | Promise<TResult>) | undefined | null): Promise<T | TResult>;
 }
