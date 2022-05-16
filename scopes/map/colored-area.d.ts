b4baabb0e3239a00593a0487ebc5368a352fad1d
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-05-16 18:50:20 +0200

    fix remove opacity

--- scopes/map/colored-area.d.ts
+++ scopes/map/colored-area.d.ts
@@ -3,7 +3,7 @@ declare namespace map {
      * Colors the ground and objects on top of it within the area defined by the positions.
     */
     class ColoredArea {
-        constructor(points: GlobalPosition[], color?: Color, border?: { color: Color, width: number } | Color, opacity?: number);
+        constructor(points: GlobalPosition[], color?: Color, border?: { color: Color, width: number } | Color);
 
         /**
           * Removes the colored area from the map.
