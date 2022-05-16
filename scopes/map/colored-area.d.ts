ee77039fa7af8f3ac02dfd6653b61b10ad171f52
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-05-16 18:46:54 +0200

    add colored area border and opacity

--- scopes/map/colored-area.d.ts
+++ scopes/map/colored-area.d.ts
@@ -3,7 +3,7 @@ declare namespace map {
      * Colors the ground and objects on top of it within the area defined by the positions.
     */
     class ColoredArea {
-        constructor(points: GlobalPosition[], color?: Color);
+        constructor(points: GlobalPosition[], color?: Color, border?: { color: Color, width: number } | Color, opacity?: number);
 
         /**
           * Removes the colored area from the map.
