d175a73f6f55b6f8306930d720d0a2604d2f5e5b
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-05-17 06:56:59 +0200

    fix data.write inheritance

--- scopes/data.write/index.d.ts
+++ scopes/data.write/index.d.ts
@@ -1,8 +1,10 @@
-declare namespace data.Variant {
-    /**
-     * Adds a colored area to a variant as a shape
-     * 
-     * The shape will now be user editable and the plugin looses control over the object.
-     */
-    function addArea(shape: map.ColoredArea): Promise<void>;
+declare namespace data {
+    interface Variant {
+        /**
+         * Adds a colored area to a variant as a shape
+         * 
+         * The shape will now be user editable and the plugin looses control over the object.
+         */
+        addArea(shape: map.ColoredArea): Promise<void>;
+    }
 }
