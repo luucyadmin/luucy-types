a605ae4ee98cbd63881c37b9385be49257660e76
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-05-27 13:52:59 +0200

    fix wms feature select parameter

--- scopes/map.layer/wms.d.ts
+++ scopes/map.layer/wms.d.ts
@@ -1,9 +1,15 @@
 declare namespace map.layer {
     class WMSLayer implements Layer {
-        constructor(name: string, url: string, layer: string, parameters: { [name: string]: string | boolean | number });
+        /**
+         * Creates an imagery layer with a web map service source
+         * 
+         * WMS layers may provide object info when selecting a point on the map.
+         * Enable `canSelectFeatures` to allow features to be selected.
+         * Beware, this might interfere with custom `onPositionSelect` handlers.
+         */
+        constructor(name: string, url: string, layer: string, parameters: { [name: string]: string | boolean | number }, canSelectFeatures?: boolean);
 
         onPositionSelect: Event<GlobalPosition>;
-        canSelectFeatures: boolean;
         opacity: number;
         hidden: boolean;
         visible: boolean;
