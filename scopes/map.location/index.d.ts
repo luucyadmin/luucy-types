71d2a671b115a78bd5dd6e8a7a10bb307f328faf
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-05-31 15:23:31 +0200

    add map.location.center

--- scopes/map.location/index.d.ts
+++ scopes/map.location/index.d.ts
@@ -1,5 +1,10 @@
 declare namespace map.location {
     /**
+     * The current center of the map
+     */
+    const center: GlobalPosition;
+
+    /**
      * Emits new center whenever the map moves around
      */
     const onCenterChange: Event<GlobalPosition>;
