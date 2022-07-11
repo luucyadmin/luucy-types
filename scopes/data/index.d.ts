bd6057fb6de7cc622cdb20fe0fa41220ee876ad6
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-07-11 14:23:11 +0200

    add section index

--- scopes/data/index.d.ts
+++ scopes/data/index.d.ts
@@ -2,6 +2,7 @@
 /// <reference path="organization.d.ts" />
 /// <reference path="project.d.ts" />
 /// <reference path="roof.d.ts" />
+/// <reference path="section.d.ts" />
 /// <reference path="usage.d.ts" />
 /// <reference path="variant.d.ts" />
 /// <reference path="wall.d.ts" />
