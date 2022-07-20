562aafcbac7ca876f288fc172f44841292cd9895
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-07-20 18:00:01 +0200

    fix add null subscriber return

--- scopes/core/event.d.ts
+++ scopes/core/event.d.ts
@@ -17,7 +17,7 @@ declare class Event<T> {
      * The handler will be executed whenever a new value is emitted with `.emit(value)` and
      * immediately after you subscribe - if there has been a `.emit(value)` call before. 
      */
-    subscribe(handler: EventHandler<T>): Subscription<T>;
+    subscribe(handler: EventHandler<T>): Subscription<T> | null;
 
     /** 
      * Subscribe once to a event
@@ -35,8 +35,8 @@ declare class Event<T> {
      * 
      * event.emit(); // hit 1, hit 2, hit 4
      */
-    subscribeOnce(tag: string, handler: EventHandler<T>): Subscription<T>;
-    subscribeOnce(handler: EventHandler<T>): Subscription<T>;
+    subscribeOnce(tag: string, handler: EventHandler<T>): Subscription<T> | null;
+    subscribeOnce(handler: EventHandler<T>): Subscription<T> | null;
 
     /** 
      * Emits a new value
