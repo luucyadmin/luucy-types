a408e0a1867b3a6c041b49aeaca6dbbf37b9e64f
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-08-01 08:10:09 +0200

    fix typo

--- scopes/map/polygon.d.ts
+++ scopes/map/polygon.d.ts
@@ -22,7 +22,7 @@ declare namespace map {
 
         readonly visible: boolean;
         readonly hidden: boolean;
-        readonly positions: GlobalPosition;
+        readonly positions: GlobalPosition[];
         
         hide(): void;
         show(): void;
