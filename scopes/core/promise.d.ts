2105c4ab03a2c6bb4714c79ee5090b3fbf1dd5d1
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-08-22 15:07:15 +0200

    fix declare on type

--- scopes/core/promise.d.ts
+++ scopes/core/promise.d.ts
@@ -1,6 +1,6 @@
-declare type PromiseConstructorLike = new <T>(executor: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void) => PromiseLike<T>;
+type PromiseConstructorLike = new <T>(executor: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void) => PromiseLike<T>;
 
-interface PromiseLike<T> {
+declare interface PromiseLike<T> {
     then<TResult1 = T, TResult2 = never>(onfulfilled: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null): Promise<TResult1 | TResult2>;
 }
 
