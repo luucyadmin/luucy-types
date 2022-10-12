4ad5af541a7ae459e28402ed2b3088ee201ecd1d
Eggsecuter <adrian.habeler@gmail.com>
2022-10-12 13:47:57 +0200

    add constructor description, fix pie chart

--- scopes/ui/charts/bar-chart.d.ts
+++ scopes/ui/charts/bar-chart.d.ts
@@ -11,10 +11,31 @@ declare namespace ui {
      * section.add(chart);
      */
     class BarChart extends Element {
+        /**
+         * The unit acts as the label of each segment
+         * Entering a simple string for the unit will append it to the value of each segment
+         * Entering a function for the unit enables to build a more complex label
+         * 
+         * ```
+         * // Outputs (200km, 150km, 35km)
+         * new ui.BarChart("Total range", "km")
+         * 
+         * // Outputs (750g, 12.5kg, 490g)
+         * new ui.BarChart("Total weight", value => value > 1000 ? `${value / 1000}kg` : `${value}g`);
+         * ```
+         * 
+         * @param name Label of the chart
+         * @param unit Unit of the segments' values
+         */
         constructor(name: string, unit?: string | ((value: number) => string));
 
         readonly name: string;
-        readonly unit: string;
+        readonly unit: string | ((value: number) => string);
+
+        /**
+         * Total value of all segments added up
+         */
+        readonly total: number;
 
         /**
          * Max value displayed in chart
@@ -22,11 +43,6 @@ declare namespace ui {
          * This may be set to a higher value than the segments, which will display a gray filler bar at the end of the chart.
          */
         max: number;
-
-        /**
-         * Total value of all segments added up
-         */
-        total: number;
 
         /**
          * Adds a new segment to the chart
--- scopes/ui/charts/pie-chart.d.ts
+++ scopes/ui/charts/pie-chart.d.ts
@@ -12,15 +12,38 @@ declare namespace ui {
      * section.add(chart);
      */
     class PieChart extends Element {
-        constructor(name: string);
+        /**
+         * The unit acts as the label of each segment
+         * Entering a simple string for the unit will append it to the value of each segment
+         * Entering a function for the unit enables to build a more complex label
+         * 
+         * ```
+         * // Outputs (200km, 150km, 35km)
+         * new ui.PieChart("Total range", "km")
+         * 
+         * // Outputs (750g, 12.5kg, 490g)
+         * new ui.PieChart("Total weight", value => value > 1000 ? `${value / 1000}kg` : `${value}g`);
+         * ```
+         * 
+         * @param name Label of the chart
+         * @param unit Unit of the segments' values
+         */
+        constructor(name: string, unit?: string | ((value: number) => string));
 
         readonly name: string;
-        readonly unit: string;
+        readonly unit: string | ((value: number) => string);
 
         /**
          * Total value of all segments added up
          */
         readonly total: number;
+
+        /**
+         * Max value displayed in chart
+         * 
+         * This may be set to a higher value than the segments, which will display a gray filler segment at the end of the chart.
+         */
+        max: number;
 
         /**
          * Adds a new segment to the chart
