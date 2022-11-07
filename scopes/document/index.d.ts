8ec68e995647c07c2cda37bc4fd8b93d469b36fc
Eggsecuter <adrian.habeler@gmail.com>
2022-11-07 10:10:33 +0100

    Fix pdf width enum

--- scopes/document/index.d.ts
+++ scopes/document/index.d.ts
@@ -1,8 +1,22 @@
 /// <reference path="pdf.d.ts" />
 
+declare enum PDFElementWidth {
+    Full = 100,
+    Half = 50
+}
+
 declare namespace document {
-    enum PDFElementWidth {
-        Full = 100,
-        Half = 50
-    }
+    /**
+     * PDF element uses full width when appended to a file (100%)
+     * 
+     * pdf.add(table, document.PDFElementWidth.Full);
+     */
+    const full: PDFElementWidth;
+
+    /**
+     * PDF element uses half of the width when appended to a file (50%)
+     * 
+     * pdf.add(table, document.PDFElementWidth.Half);
+     */
+    const half: PDFElementWidth;
 }
