ad664235b98760bc3eea356059c77fcf4751a7a0
kucerajk <jakub.kucera@email.com>
2023-05-29 11:10:19 +1200

    Update generateCSV method

--- scopes/document/csv.d.ts
+++ scopes/document/csv.d.ts
@@ -8,23 +8,24 @@ declare namespace document {
     /**
      * Creates a CSV file from given table
      *
-     * @param name filename
+     * @param name filename (if not provided the .csv suffix will be added)
      * @param data table data hodling the values for generate CSV
-     * @param format (default csv) output file format (currently only CSV is allowed)
      * @param delimiter (default , ) CSV delimiter character
      * @param addHeader (default false) flag if the header of the table should be added to result CSV file
      *
-     * @example // Create an .obj file and download it
-     * const csvFile = CSV.generateCSV("results",data, "csv", ", true);
+     * @example // Create an .csv file and download it
+     * const tableMulti = new ui.Table([],[new ui.Column("item", item => item.name), new ui.Column("cost", item => item.cost)]);
+     * tableMulti.setRecords([{name: 'tomato', cost: 30},{name: 'apple', cost: 25},{name: 'bun', cost: 5}]);
+     *
+     * const csvFile = document.CSV.generateCSV("test", tableMulti,',',true);
      *
      * section.add(new ui.Button(ui.icons.export, "Download CSV", () => {
-     *     ui.download(file);
+     *     ui.download(csvFile);
      * }));
      */
     static generateCSV(
       name: string,
       data: ui.Table<any>,
-      format?: string,
       delimiter?: string,
       addHeader?: boolean
     ): File;
