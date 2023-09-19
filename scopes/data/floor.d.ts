a1bd87a0ca934b12f30d929722a4c8242a897e7f
kucerajk <jakub.kucera@email.com>
2023-09-19 21:16:34 +0200

    Update floor

--- scopes/data/floor.d.ts
+++ scopes/data/floor.d.ts
@@ -14,11 +14,6 @@ declare namespace data {
         readonly index: number;
 
         /**
-         * Name of the floor
-         */
-        readonly name: string;
-
-        /**
          * Color of the floor
          */
         readonly color: Color;
