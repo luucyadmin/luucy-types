46e05edabd2fd52916b44a9a7e20d260cca77533
kucerajk <jakub.kucera@email.com>
2023-09-26 20:27:58 +1300

    Add border to polygon

--- scopes/data.write/index.d.ts
+++ scopes/data.write/index.d.ts
@@ -2,14 +2,14 @@ declare namespace map {
     interface ColoredArea {
         /**
          * Adds the colored area to a variant as a shape
-         * 
+         *
          * The shape will now be user editable and the plugin looses control over the object.
          */
         releaseToVariant(name: string): Promise<void>;
 
         /**
          * Adds the colored area to a variant as a shape
-         * 
+         *
          * The shape will now be user editable and the plugin looses control over the object.
          */
         releaseAsParcelToVariant(name: string): Promise<void>;
@@ -18,10 +18,19 @@ declare namespace map {
     interface Line {
         /**
          * Adds the line to a variant as a shape
-         * 
+         *
          * The shape will now be user editable and the plugin looses control over the object.
          */
-         releaseToVariant(name: string): Promise<void>;
+        releaseToVariant(name: string): Promise<void>;
+    }
+
+    interface Polygon {
+        /**
+         * Adds the polygon to a variant as a new shape
+         *
+         * The shape will now be user editable and the plugin looses control over the object.
+         */
+        releaseToVariant(name: string): Promise<void>;
     }
 }
 
@@ -30,10 +39,13 @@ declare namespace data {
         /**
          * Creates a new variant in the project and returns it
          */
-        createVariant(name: string, drawing?: { 
-            edges: boolean, 
-            sketch: boolean, 
-            usages: boolean 
-        }): Promise<Variant>;
+        createVariant(
+            name: string,
+            drawing?: {
+                edges: boolean;
+                sketch: boolean;
+                usages: boolean;
+            }
+        ): Promise<Variant>;
     }
 }
--- scopes/map/polygon.d.ts
+++ scopes/map/polygon.d.ts
@@ -24,6 +24,7 @@ declare namespace map {
         readonly hidden: boolean;
         readonly points: GlobalPosition[];
         readonly height: number;
+        readonly border?: { color: Color, width?: number };
         
         hide(): void;
         show(): void;
