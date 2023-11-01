77536d78d0251ae97e6db94efa7bc9f6e73004e3
kucerajk <jakub.kucera@email.com>
2023-11-01 17:19:37 +1300

    Move buffer polygon to the map functions

--- scopes/map/index.d.ts
+++ scopes/map/index.d.ts
@@ -7,10 +7,10 @@
 
 /**
  * Interactions with luucys main map.
- * 
+ *
  * @example // Creating a marker and a button to focus it
  * const marker = new map.Marker(new GlobalPosition(47.0469494, 8.3157369));
- * 
+ *
  * section.add(new ui.Button(ui.icons.pointOfInterest, "Focus Marker", () => {
  *     marker.focus();
  * }));
@@ -18,20 +18,51 @@
 declare namespace map {
     /**
      * Focuses multiple elements at once. The map will zoom out to try to show all elements at once.
-     * 
+     *
      * Do not focus a element without any user interaction!
      */
-    function focus(elements: (ColoredArea | Line | Marker | Polygon | GlobalPosition | Label)[]): void;
+    function focus(
+        elements: (
+            | ColoredArea
+            | Line
+            | Marker
+            | Polygon
+            | GlobalPosition
+            | Label
+        )[]
+    ): void;
 
     /**
      * On Position Select
-     * 
+     *
      * The event will be emitted when the user clicks somewhere on the map.
-     * 
+     *
      * @example // Add a marker where the user clicked
      * map.onPositionSelect.subscribe(position => {
      *     new map.Marker(position.flattenedCopy(), Color.random());
      * });
      */
     const onPositionSelect: Event<GlobalPosition>;
+
+    /**
+     * Returns buffered polygon points if succeeded, undefined if failed (can occur when buffer distance < 0 -> buffering inside so that the new polygon intersects with itself)
+     *
+     * @returns array of global position for each polygon (in case of intersection the provided points may be transformed to two separated polygons hence the array response).
+     * For most of the cases the response will be array having the length of 1
+     * 
+     * @example
+     * const buffer = map.bufferPolygon([
+     *     new GlobalPosition(47.050390, 8.310263),
+     *     new GlobalPosition(47.050390, 8.311263),
+     *     new GlobalPosition(47.051390, 8.311263),
+     *     new GlobalPosition(47.051390, 8.310263),
+     * ], 5, 10);
+     * 
+     * const polyfon = buffer[0];
+     */
+    function bufferPolygon(
+        points: GlobalPosition[],
+        bufferDistance: number,
+        areaScaleFactor: number
+    ): [GlobalPosition[]];
 }
