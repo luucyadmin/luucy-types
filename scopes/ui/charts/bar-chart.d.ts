93800f3049f48d0b53ea8a724b7cfa231fe934b0
Jakub Kučera <kucikj@gmail.com>
2024-03-14 02:04:27 +0000

    Feature/lu 7155 tooltip

--- scopes/ui/charts/bar-chart.d.ts
+++ scopes/ui/charts/bar-chart.d.ts
@@ -26,8 +26,9 @@ declare namespace ui {
          * 
          * @param name Label of the chart
          * @param unit Unit of the segments' values
+         * @param help - Will provide tooltip help on mouseover
          */
-        constructor(name: string, unit?: string | ((value: number) => string));
+        constructor(name: string, unit?: string | ((value: number) => string), help?: string);
 
         readonly name: string;
         readonly unit: string | ((value: number) => string);
