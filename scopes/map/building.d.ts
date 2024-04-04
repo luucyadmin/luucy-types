a58b7dc5456263e0c1de116dee567d5851a1cd8b
Jakub Kučera <kucera@zensys.cz>
2024-04-04 09:17:30 +0000

    Feature/lu 6893 b2 onboard

--- scopes/map/building.d.ts
+++ scopes/map/building.d.ts
@@ -24,6 +24,10 @@ declare namespace map {
         readonly visible: boolean;
         readonly hidden: boolean;
         readonly floors: data.BuildingFloor[];
+        /**
+         * Define the roof section of the building
+         * Overrides values defined in constructor
+         */
         roof?: data.RoofSection;
         /**
          * Define fill color for the building
@@ -31,10 +35,70 @@ declare namespace map {
         color?: Color;
 
         /**
+         * Define building opacity in %
+         * Default is 100
+         */
+        opacity?: number;
+
+        /**
+         * Define overground floor height
+         * Default is 3.2
+         * 
+         * Does override floorHeight for the overground floors
+         */
+        floorHeight?: number;
+
+        /**
+         * Define underground floor height
+         * Default is 3.2
+         * 
+         * Does override floorHeight for the underground floors
+         */
+        undergroundFloorHeight?: number;
+        
+        /**
+         * Define building area reduction
+         * Default is 0
+         */
+        areaReduction?: number;
+
+        /**
+         * Define building volume reduction
+         * Default is 0
+         */
+        volumeReduction?: number;
+
+        /**
+         * Define building usage
+         */
+        buildingUsage?: data.BuildingUsage;
+
+
+        /**
          * Enabled displaying floors in the building
          * @param show
          */
         showFloors(show: boolean): void;
+
+        /**
+         * Adds a overground floor
+         * @param floor to be added - if not defined the first overground floor will be used as a reference
+         */
+        addFloor(floor?: data.BuildingFloor): void;
+
+        /**
+         * Adds a underground floor 
+         * @param floor to be added - if not defined the first underground floor will be used as a reference
+         */
+        addUndergroundFloor(floor?: data.BuildingFloor): void;
+
+        /**
+         * Remove defined floor
+         * @param index floor index from the building floors array
+         */
+        removeFloor(index: number): void;
+
+
         
         hide(): void;
         show(): void;
