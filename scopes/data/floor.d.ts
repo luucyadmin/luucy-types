48c01683027329f81af671ed834b57d5f7391458
kucerajk <jakub.kucera@email.com>
2024-04-16 08:45:47 +1200

    Update floor

--- scopes/data/floor.d.ts
+++ scopes/data/floor.d.ts
@@ -1,9 +1,30 @@
-
-
 declare namespace data {
+    /**
+     * BuildingFloor
+     *
+     * A building floor is a part of the building
+     *
+     * @example // Create a simple building floor with 4 points (100x100 meters) at specific location defined by center point
+     * const points = [
+     *    new LocalPosition(50, 50),
+     *    new LocalPosition(50, -50),
+     *    new LocalPosition(-50, -50),
+     *    new LocalPosition(-50, 50)
+     * ];
+     * const floor = new data.BuildingFloor(
+     *     new GlobalPosition(47.050390, 8.310263),points, data.overground, 2.85, Color.red);
+     *
+     * @see map.Building for how to use BuildingFloor for Building creating
+     */
     class BuildingFloor {
-
-        constructor(positions: LocalPosition[], floorType?: FloorType, height?: number, color?: Color, usage?: BuildingUsage)
+        constructor(
+            centerPoint: GlobalPosition,
+            points: LocalPosition[],
+            floorType?: FloorType,
+            height?: number,
+            color?: Color,
+            usage?: BuildingUsage
+        );
 
         /**
          * Floor identifier
@@ -36,8 +57,13 @@ declare namespace data {
         usage?: BuildingUsage;
 
         /**
+         * The center location of the floor
+         */
+        centerPoint: GlobalPosition;
+
+        /**
          * Floor points
          */
-        positions?: LocalPosition[];
+        points?: LocalPosition[];
     }
 }
