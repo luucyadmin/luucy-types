c1cb9103ae974fad854b99b48b6dee6f4422b236
kucerajk <jakub.kucera@email.com>
2025-01-24 12:44:08 +1300

    LU-7821 - revert illegal interfaces

---
+++ globals/illegals.d.ts
@@ -0,0 +1,9 @@
+/** 
+ * Function constructor. Will not compile 
+ */
+declare interface Function {}
+
+/** 
+ * Function constructor arguments: Will not compile 
+ */
+declare interface IArguments {}
