be40c1aefa9d97a5e3aaff4a639bacfc08f6540f
Jakub kucera <kucera@zensys.cz>
2025-02-20 22:18:54 +0100

    Next/3.9

---
+++ scopes/data/building-floor.d.ts
@@ -0,0 +1,40 @@
+declare namespace data {
+    /**
+     * BuildingFloor
+     *
+     * A building floor is a part of the building block
+     *
+     * @example // Create a simple building floor
+     * const floor = new data.BuildingFloor(data.overground, 2.85);
+     * block.floors.push(floor)
+     *
+     * @see data.BuildingBlock for how to use BuildingFloor for Building creating
+     */
+    class BuildingFloor {
+        constructor(
+            floorType: FloorType,
+            height: number,
+        );
+
+        /**
+         * Color of the floor
+         */
+        color?: Color;
+
+        /**
+         * Height of the floor
+         */
+        height: number;
+
+        /**
+         * Type of the floor
+         */
+        floorType: FloorType;
+
+        /**
+         * Floor usage
+         */
+        usage?: BuildingUsage;
+
+    }
+}
